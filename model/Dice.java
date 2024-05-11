00aec0f094e81b3de2920af058d6c0d9e5b9ebcf
Agapitus <89057298+Agapitus1@users.noreply.github.com>
2024-05-11 23:06:50 +1000

    Add files via upload

---
+++ model/Dice.java
@@ -0,0 +1,25 @@
+package model;
+
+
+public class Dice extends Updater //model inherits from Updater
+{
+    private final int SIDES = 6;
+    private int roll;//date is stored by the object
+    
+    /**
+     * Constructor for objects of class Dice
+     */
+    public Dice()
+    {
+    }
+    
+    public void roll()//procedures that change data MUST call updateViews()
+    {
+       roll = (int) ((Math.random()*SIDES)+1);
+       updateViews();//calls update for each observer
+    }
+    public int getRoll()//window calls accessor to refresh the data
+    {
+        return roll;
+    }
+}
